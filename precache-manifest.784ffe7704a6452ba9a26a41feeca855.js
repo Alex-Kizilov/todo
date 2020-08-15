9c6f20fc682e72da36b3801b7964868fe607443b
Alex-Kizilov <62868932+Alex-Kizilov@users.noreply.github.com>
2020-08-15 05:51:52 +0500

    Add files via upload

---
+++ precache-manifest.784ffe7704a6452ba9a26a41feeca855.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9bb90749fdca4061f9cc9b4b045523c",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "e4e246584c35fe97c65c",
+    "url": "/todo/static/css/main.f1e48f80.chunk.css"
+  },
+  {
+    "revision": "be19641d3fe78d35223f",
+    "url": "/todo/static/js/2.ef8b4283.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.ef8b4283.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4e246584c35fe97c65c",
+    "url": "/todo/static/js/main.dd7230ca.chunk.js"
+  },
+  {
+    "revision": "da7c3f7f7e5963fe74b8",
+    "url": "/todo/static/js/runtime-main.74315f96.js"
+  }
+]);
